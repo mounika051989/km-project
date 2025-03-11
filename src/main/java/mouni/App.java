5a066e19c28100be6153440767ea6ae5beb27c23
mounika <mounika.palla@gmail.com>
2025-03-11 19:12:40 +0000

    changed to kethan

--- src/main/java/mouni/App.java
+++ src/main/java/mouni/App.java
@@ -5,6 +5,6 @@ package mouni;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello kadapa!");
+        System.out.println("Hello kethan!");
     }
 }
