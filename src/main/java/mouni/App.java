6b4101793317791497cf969e90bf463e31e0133c
mahesh <mounika.palla@gmail.com>
2025-03-11 19:33:02 +0000

    changed to manjith

--- src/main/java/mouni/App.java
+++ src/main/java/mouni/App.java
@@ -5,6 +5,6 @@ package mouni;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello kethan!");
+        System.out.println("Hello manjith!");
     }
 }
