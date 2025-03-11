e40a2fc0f3d19f65bed8fd9e4c6bfac378e5f01b
mahesh <mounika.palla@gmail.com>
2025-03-11 19:40:44 +0000

    changed the commit

--- src/main/java/mouni/App.java
+++ src/main/java/mouni/App.java
@@ -5,6 +5,6 @@ package mouni;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello how are you!");
+        System.out.println("i am fine!");
     }
 }
